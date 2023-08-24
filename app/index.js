c9b8c953f93b3baf47daff0a7d2605da06fd0ad0
Kent Erik <kent.erik.hole@outlook.com>
2023-08-24 14:14:58 +0200

    bug #001 try to replace the item inside a html

--- app/index.js
+++ app/index.js
@@ -1,20 +1,29 @@
 const fs = require("fs")
 const http = require("http")
+const path = require('path')
 const url = require("url")
 
+const replaceTemplet = require('./module/replaceTemplets'); 
 
+// Read File Sync
+// data.json
 const data = fs.readFileSync(`${__dirname}/apidata/data.json`, "utf-8")
 const dataObj = JSON.parse(data)
-
+// Read Templet 
+    // Overveiw 
+    const tempOverveiw = fs.readFileSync(`${__dirname}/templetes/templet-overview.html`)
+    // Card
+    const tempCard = fs.readFileSync(`${__dirname}/templetes/templet-card.html`)
+    
 const server = http.createServer((req, res) => {
     const {query, pathname} = url.parse(req.url, true)
-
+    console.log(req.url)
     if(pathname === "/" || pathname === "overview") {
         res.writeHead(200, {"Content-type": "text/html"});
-        const card = dataObj.map(el => replaceTemplet(tempCard, el)).join("")
-        const output = tempOverview.replace("{%PRODUCT_CARDS}", card)
+        const card = dataObj.map(el => replaceTemplet(tempCard, el)).join('')
+        //const output = tempOverveiw.replace("{%DOCKER_CARDS}", card)
 
-        res.end(output)
+        res.end(card)
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
