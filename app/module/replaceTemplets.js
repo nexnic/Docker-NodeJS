63b160bfd27a2a11fb0e70da03af0e918c9485ec
Kent Erik <kent.erik.hole@outlook.com>
2023-08-24 14:46:57 +0200

    adding replaceTemplet

--- app/module/replaceTemplets.js
+++ app/module/replaceTemplets.js
@@ -2,7 +2,8 @@ module.exports = (temp, items) => {
     // console.log(temp , items)
     
     let output = temp.replace(/{%DOCKERNAME%}/g, items.containerName)
-    //output = output.replace(/{%IMAGE%}/g, items.image)
-    //output = output.replace(/{%DESCRIPTION%}/g, items.description)
+    output = output.replace(/{%IMAGE%}/g, items.image)
+    output = output.replace(/{%DESCRIPTION%}/g, items.description)
+    output = output.replace(/{%LINK%}/g, items.link)
     return output
 }
