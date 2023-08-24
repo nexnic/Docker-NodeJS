2ae1a1856d885b05c428fc8e05151a77b711d845
Kent Erik <kent.erik.hole@outlook.com>
2023-08-24 14:15:41 +0200

    replace Templet

--- app/module/replaceTemplets.js
+++ app/module/replaceTemplets.js
@@ -1,6 +1,8 @@
 module.exports = (temp, items) => {
-    let output = temp.replace(/{%DOCKERNAME%}/g, items.dockername)
-    output = output.replace(/{%IMAGE%}/g, items.image)
-    output = output.replace(/{%DESCRIPTION%}/g, items.description)
+    // console.log(temp , items)
+    
+    let output = temp.replace(/{%DOCKERNAME%}/g, items.containerName)
+    //output = output.replace(/{%IMAGE%}/g, items.image)
+    //output = output.replace(/{%DESCRIPTION%}/g, items.description)
     return output
 }
