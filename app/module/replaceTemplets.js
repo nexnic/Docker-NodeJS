baf25bd8174ceee7e0e512e88cfea85f273e63ab
Kent Erik <kent.erik.hole@outlook.com>
2023-08-25 20:06:24 +0200

    remove testing console

--- app/module/replaceTemplets.js
+++ app/module/replaceTemplets.js
@@ -1,5 +1,4 @@
 module.exports = (temp, items) => {
-    // console.log(temp , items)
     
     let output = temp.replace(/{%DOCKERNAME%}/g, items.containerName)
     output = output.replace(/{%IMAGE%}/g, items.image)
